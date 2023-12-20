64eec419032d41ce78fa89d2ea50f7be3a0d5fed
Roobon <roobon@gmail.com>
2023-12-20 13:23:21 +0600

    Class work 20 dec

--- ci_project1/app/Controllers/frontend/RegistrationController.php
+++ ci_project1/app/Controllers/frontend/RegistrationController.php
@@ -3,8 +3,11 @@
 namespace App\Controllers\Frontend;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 use CodeIgniter\RESTful\ResourceController;
 use CodeIgniter\API\ResponseTrait;
+
+
 
 class RegistrationController extends BaseController
 {
@@ -12,7 +15,31 @@ class RegistrationController extends BaseController
     
     public function registration()
     {
+        $userModel = new UserModel();
+        $json = $this->request->getVar();
+        //$data = json_decode($json); 
+        //print_r($json);
+        $data = [
+            'name' => $json['fname'],
+            'email' => $json['email'],
+            'password' => $json['password'],
+        ];
+
         $data['msg'] = "Success";
+    
+               
+        // if($userModel->insert($data)){
+        //     $data['msg'] = "Success";
+        //     return $this->respond($data);
+        // } else {
+        //     $data['msg'] = "Failed";
+        //     return $this->respond($data);
+        // }
+         $userModel->save($data);
+     
+       
         return $this->respond($data);
+
+        
     }
 }
